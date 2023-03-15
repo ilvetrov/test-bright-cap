1b3c1cc8bbc85ef27e5a1201e152b4d61d3e4b66
ilvetrov <ilya.ilvetrov@gmail.com>
2023-03-15 14:37:35 +0400

    test husky

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
 import styles from './Home.module.scss'
 
 export default function Home() {
-  return <div className={styles.title}>Let’s Connect</div>
+  return <div className={styles.title}>Let’s Connect </div>
 }
