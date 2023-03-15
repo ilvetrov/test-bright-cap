68b5bb764a2fec420586c46ddca5c6e278d4f24f
ilvetrov <ilya.ilvetrov@gmail.com>
2023-03-15 14:36:47 +0400

    init

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+export default function Logo() {
+  return (
+    <img
+      src="/img/logo.svg"
+      alt="BrightCap"
+      loading="lazy"
+      decoding="async"
+      width="652"
+      height="198"
+    />
+  )
+}
